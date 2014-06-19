b27da6d1673adf1374bf4c37372a7ed01d5badbc
Hasso Pape <hasso_pape@sil.org>
2014-06-19 14:26:21 -0500

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿using Gecko;
+
+namespace GeckoGrabsFocus
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.webBrowser1 = new Gecko.GeckoWebBrowser();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(29, 230);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// webBrowser1
+			// 
+			this.webBrowser1.Location = new System.Drawing.Point(8, 8);
+			this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowser1.Name = "webBrowser1";
+			this.webBrowser1.Size = new System.Drawing.Size(250, 186);
+			this.webBrowser1.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.webBrowser1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private Gecko.GeckoWebBrowser webBrowser1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Gecko;
+
+namespace GeckoGrabsFocus
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			// Initialize XULRunner - required to use the geckofx WebBrowser Control (GeckoWebBrowser)
+			string xulRunnerLocation;
+			if (Environment.OSVersion.Platform == PlatformID.Unix)
+			{
+				xulRunnerLocation = XULRunnerLocator.GetXULRunnerLocation();
+				if (String.IsNullOrEmpty(xulRunnerLocation))
+					throw new ApplicationException("The XULRunner library is missing or has the wrong version");
+				string librarySearchPath = Environment.GetEnvironmentVariable("LD_LIBRARY_PATH") ?? String.Empty;
+				if (!librarySearchPath.Contains(xulRunnerLocation))
+					throw new ApplicationException("LD_LIBRARY_PATH must contain " + xulRunnerLocation);
+			}
+			else
+			{
+				xulRunnerLocation = Path.GetFullPath("xulrunner");
+				if (!Directory.Exists(xulRunnerLocation))
+					throw new ApplicationException("XULRunner needs to be installed to " + xulRunnerLocation);
+			}
+
+			Xpcom.Initialize(xulRunnerLocation);
+			GeckoPreferences.User["gfx.font_rendering.graphite.enabled"] = true;
+
+			InitializeComponent();
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			webBrowser1.LoadContent(textBox1.Text, "file:///c:/MayNotExist/doesnotmatter.html", "application/xhtml+xml");
+			webBrowser1.Refresh();
+		}
+	}
+}
